24c5a916cf9c0b09948934efbdc5d1c6d1060c49
brotherfashionhouse22-art <brotherfashionhouse22@gmail.com>
2026-01-13 21:31:43 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,6 @@
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-storage-compat.js"></script>
-
-<script>
+// Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD56Psy7pNVXKXphbSwgAJ6LkUb33yb2iQ",
+  apiKey: "AIzaSyD56Psy7pNVXXKXphb3bAL6kUb33vb2iK",
   authDomain: "brother-fashion-house.firebaseapp.com",
   projectId: "brother-fashion-house",
   storageBucket: "brother-fashion-house.appspot.com",
@@ -12,7 +8,8 @@ const firebaseConfig = {
   appId: "1:30910144708:web:4d277b3079432b85ae6991"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Firestore
 const db = firebase.firestore();
-const storage = firebase.storage();
-</script>
